381a1b7f4ba34df04555adba0b5e86697d815299
ryanscottjohnson <teacherman1@gmail.com>
2018-10-29 11:17:27 -0700

    basic hello app in react native

---
+++ app.js
@@ -0,0 +1,12 @@
+import React, { Components } from 'react';
+import { Text, View } from 'react-native';
+
+export default class HelloWorld extends Components {
+  render() {
+    return (
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
+}
